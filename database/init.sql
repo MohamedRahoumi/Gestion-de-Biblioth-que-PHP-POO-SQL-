eac7d648938f1c7790f76e601ce2b3024da518a2
MohamedRahoumi <mohamedrahoumi4@gmail.com>
2025-12-28 23:07:36 +0100

    les requtte sql

---
+++ database/init.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS library_db;
+USE library_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('reader','admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(150) NOT NULL,
+    year INT NOT NULL,
+    status ENUM('available','borrowed') NOT NULL DEFAULT 'available',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS borrows (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    readerId INT NOT NULL,
+    bookId INT NOT NULL,
+    borrowDate DATETIME NOT NULL,
+    returnDate DATETIME NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (readerId) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (bookId) REFERENCES books(id) ON DELETE CASCADE
+);
+INSERT INTO books (title, author, year, status) VALUES
+('Le Petit Prince', 'Antoine de Saint-Exupéry', 1943, 'available'),
+('Les Misérables', 'Victor Hugo', 1862, 'available'),
+('L\'Étranger', 'Albert Camus', 1942, 'available'),
+('Madame Bovary', 'Gustave Flaubert', 1857, 'available'),
+('Germinal', 'Émile Zola', 1885, 'available');
+-- INSERT INTO users (firstName, lastName, email, password, role) VALUES
+-- ('Admin', 'System', 'admin@library.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin');
+
